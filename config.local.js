90e4d5b4c09dd9e6228e53d74cc75585e9b5ff34
Nguyễn Thanh Hân <nguyenthanhhan.com@gmail.com>
2026-07-22 10:30:16 +0200

    Remove local Supabase config from repository

--- config.local.js
+++
@@ -1,5 +0,0 @@
-window.DHL_CONFIG = {
-  supabaseUrl: 'https://iytgmqltsbxrdblwudsq.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5dGdtcWx0c2J4cmRibHd1ZHNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMDIyNTUsImV4cCI6MjA5OTY3ODI1NX0.b7476o-AQr62yvDPWhPr6dxOXIFTaQUAciW5qPL1-QA',
-};
-
